01745c0e494755dc61ebd2fc97d58502996d4f71
buddemat <buddemat@users.noreply.github.com>
2025-01-10 08:28:24 -0500

    Feat: added server code for flask deployment of emotion detection (task 6).

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import run_watson_emotion_detection as emo
+
+app = Flask('Emotion Detection')
+
+@app.route('/')
+def show_index_page():
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def run_emotion_detection():
+    text_to_analyze = request.args.get('textToAnalyze')
+    result_json = emo(text_to_analyze)
+    dominant_emotion = result_json.pop('dominant_emotion')
+    return f'For the given statement, the system response is {str(result_json).strip("{}")}. The dominant emotion is <b>{dominant_emotion}</b>.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', 
+            port=5000,
+            debug=True)
